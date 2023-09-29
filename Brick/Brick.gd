be80ccd11916c793d19f0aed4ab8d808a5681b95
AlexKutza <adkutza@gmail.com>
2023-09-28 23:53:42 -0400

    Project 3 part 1

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -11,7 +11,7 @@ func _ready():
 	position = new_position
 
 func _physics_process(_delta):
-	if dying:
+	if dying and not $Shapes.emitting:
 		queue_free()
 
 func hit(_ball):
@@ -20,7 +20,8 @@ func hit(_ball):
 func die():
 	dying = true
 	collision_layer = 0
-	$ColorRect.hide()
+	$Brick.hide()
+	$Shapes.emitting = true
 	Global.update_score(score)
 	if not Global.feverish:
 		Global.update_fever(score)
